cd203ca019d27b9fec91e3194a4efdfe1c47c0cb
maxim <maksppap123@mail.ru>
2022-12-04 16:30:31 +0300

    init

---
+++ scripts/read_write_script.py
@@ -0,0 +1,22 @@
+import json
+import yaml
+import os
+
+
+def read_json(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def save_json(path, data):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4,ensure_ascii=False)
+
+
+def read_yaml(path):
+    with open(path, "r") as stream:
+        try:
+            data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return data
